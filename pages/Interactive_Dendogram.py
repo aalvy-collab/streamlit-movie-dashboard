f85cbbe044eca13078dc19b6a1e494013e3699f0
aalvy-collab <sba24433@student.cct.ie>
2025-05-21 23:54:43 +0100

    code updated for Streamlit

--- pages/Interactive_Dendogram.py
+++ pages/Interactive_Dendogram.py
@@ -3,17 +3,20 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import scipy.cluster.hierarchy as sch
+from scipy.cluster.hierarchy import leaves_list
 
-# Page configuration
+# Page config
 st.set_page_config(page_title="Interactive Dendrogram", layout="wide")
+st.title("Interactive Dendrogram")
+st.markdown("Visualizing hierarchical similarity between movie clusters based on genre distributions.")
 
-# Load dataset
+# Load data
 df = pd.read_csv("cluster_final.csv")
 genre_columns = df.columns[3:]
 data = df[genre_columns].values
 
-# Cluster Labels
-labels = [
+# Labels
+cluster_labels = [
     "Horror Fans", "Comedy Fans", "Family Friendly",
     "Drama Buffs", "Action Junkies", "Doc Watchers", "History Fiends"
 ]
@@ -21,17 +24,19 @@ labels = [
 # Linkage matrix
 linkage_matrix = sch.linkage(data, method='ward')
 
-# Dendrogram without plotting
-dendro = sch.dendrogram(linkage_matrix, labels=labels, orientation='top', no_plot=True)
+# Match leaf order to linkage
+leaf_order = leaves_list(linkage_matrix)
+ordered_labels = [cluster_labels[i] for i in leaf_order]
 
+# Dendrogram without plot
+dendro = sch.dendrogram(linkage_matrix, labels=ordered_labels, orientation='top', no_plot=True)
 icoord = np.array(dendro['icoord'])
 dcoord = np.array(dendro['dcoord'])
-leaf_labels = dendro['ivl']
 
 # Create Plotly figure
 fig = go.Figure()
 
-# Add dendrogram lines
+# Add lines
 for i in range(len(icoord)):
     fig.add_trace(go.Scatter(
         x=icoord[i],
@@ -41,25 +46,25 @@ for i in range(len(icoord)):
         hoverinfo='none'
     ))
 
-# Update layout
+# Style layout
 fig.update_layout(
-    title="Interactive Dendrogram – Cluster Similarity by Genre",
+    title="Cluster Similarity by Genre Profile",
     xaxis=dict(
         tickvals=[(x[1] + x[2]) / 2 for x in icoord],
-        ticktext=leaf_labels,
-        tickfont=dict(size=12),
-        title='Clusters'
+        ticktext=ordered_labels,
+        tickangle=45,
+        title='Clusters',
+        tickfont=dict(size=12)
     ),
     yaxis=dict(
         title='Distance',
         tickfont=dict(size=12)
     ),
     plot_bgcolor='white',
-    margin=dict(l=60, r=20, t=60, b=80),
-    height=500
+    margin=dict(l=60, r=40, t=60, b=80),
+    height=550,
+    showlegend=False,
+    font=dict(family='Poppins', size=14)
 )
 
-# Render in Streamlit
-st.title("Interactive Dendrogram")
-st.markdown("Visualizing hierarchical similarity between movie clusters based on genre distributions.")
 st.plotly_chart(fig, use_container_width=True)
