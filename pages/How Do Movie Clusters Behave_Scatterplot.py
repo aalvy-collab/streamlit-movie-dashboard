e0f9135bdb78dbe8bdcd9604feab751081822fb5
aalvy-collab <sba24433@student.cct.ie>
2025-05-21 23:30:59 +0100

    Libraries updated for Streamlit compatability

--- pages/How Do Movie Clusters Behave_Scatterplot.py
+++ pages/How Do Movie Clusters Behave_Scatterplot.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.graph_objects as go
-from matplotlib.colors import rgb2hex 
 import numpy as np
 import pandas as pd
 import numpy as np
